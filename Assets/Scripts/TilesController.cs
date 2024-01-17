6a30c9ff112cd1bad08066e604005ac6a42cd2f5
Jerri Ahonen <jerri.ahonen@gmail.com>
2024-01-17 22:10:30 +0200

    new movement wip

--- Assets/Scripts/TilesController.cs
+++ Assets/Scripts/TilesController.cs
@@ -4,14 +4,38 @@ using UnityEngine;
 
 public class TilesController : MonoBehaviour
 {
-	private HashSet<Vector3Int> tileCoords = new();
+	private readonly HashSet<Vector3Int> tileCoords = new();
+
+	private Grid<object> grid;
 
 	private void Awake()
 	{
+		float minX = float.MaxValue; 
+		float minZ = float.MaxValue;
+		float maxX = float.MinValue;
+		float maxZ = float.MinValue;
+		
 		foreach (Transform t in transform)
 		{
 			tileCoords.Add(new Vector3Int((int)t.position.x, (int)t.position.y, (int)t.position.z));
+			if (t.position.x < minX) minX = t.position.x;
+			if (t.position.x > maxX) maxX = t.position.x;
+			if (t.position.z < minZ) minZ = t.position.z;
+			if (t.position.z > maxZ) maxZ = t.position.z;
 		}
+
+		var gridSizeX = maxX - minX;
+		var gridSizeZ = maxZ - minZ;
+		var gridSize = new Vector2Int((int)gridSizeX + 1, (int)gridSizeZ + 1);
+		var minCorner = new Vector2(minX, minZ);
+		var maxCorner = new Vector2(maxX, maxZ);
+		var origin = minCorner;
+		grid = new Grid<object>(gridSize, 1f, origin, true, false, (Vector2Int gridPos, Vector3 worldPos, int index) => new object());
+	}
+
+	private void OnDrawGizmos()
+	{
+		grid?.OnDrawGizmos_DrawDebugData();
 	}
 
 	public bool IsTile(Vector3Int pos)
